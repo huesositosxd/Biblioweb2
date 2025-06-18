053e4e71726cb4e11dd20ebe5aeb5a7b2e69df4b
Josue Egremi <josueegremi@gmail.com>
2025-06-17 23:04:22 -0600

    Estructura

--- BiblioParque/settings.py
+++ BiblioParque/settings.py
@@ -64,7 +64,7 @@ ROOT_URLCONF = 'BiblioParque.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'Aplicaciones' / 'Visitas' / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
